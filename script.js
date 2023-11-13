89bc7c70ecebcbbfa6d2da80153f68f6ff294618
Aatu Saikkonen <Saikkonen@users.noreply.github.com>
2023-11-13 12:05:50 +0200

    Javascript function to update results on page

---
+++ script.js
@@ -0,0 +1,15 @@
+function haeRenkaat() {
+  var xhr = new XMLHttpRequest()
+  var koko = document.getElementById('renkaanKoko').value
+  var tyyppi = document.getElementById('renkaanTyyppi').value
+  var url = 'search.php'
+
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      document.getElementById('renkaat').innerHTML = xhr.responseText
+    }
+  }
+  xhr.send('renkaanKoko=' + koko + '&renkaanTyyppi=' + tyyppi)
+}
